3c7d83850b030837c7ef7ba834ca224ce39c9c86
Bhaveh shimpi <bvsram44@gmail.com>
2018-11-17 21:00:02 +0530

    travis integration

--- src/app/unitTestCase.spec.ts
+++ src/app/unitTestCase.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {MainService} from './service/main.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA  } from '@angular/core';
+import{ LoginComponent } from './public/login/login.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -13,8 +15,43 @@ describe('AppComponent', () => {
       imports: [RouterTestingModule]
     }).compileComponents();
   }));
-  it('says hello', () => {
+  it('Check says hello', () => {
     let obj = new AppComponent();
     expect(obj.helloWorld()).toEqual("Hello world!");
+  });
+});
+
+describe('MainService', () => {
+  let service: MainService;
+  beforeAll(() => {
+    service = new MainService(null, null, null);
+  });
+
+  it("Check Network Status", () => {
+    expect(service.checkNetworkStatus()).toBeTruthy();
+  });
+
+  it("Check User logut by removing local storage", () => {
+
+    service.removeUserDetails();
+    expect(service.getToken()).toBeNull();
+  });
+
+  it("Check User login by default", () => {
+    expect(service.getToken()).toBeNull();
+  });
+
+  it("Check User login by setting token value", () => {
+    var user = {
+      "token": "testToken",
+      "name": "testName",
+      "email": "testEmail"
+    }
+    service.setUserDetails(user);
+    expect(service.getToken()).toEqual("testToken");
+  });
+
+  it("Check Base url define", () => {
+    expect(service.BASE_URL).toBeDefined();
   });
 });
